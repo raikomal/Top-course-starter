1dde99accb4f3e1578a41543c88610566016e6bc
raikomal <raikomal62@gmail.com>
2025-06-18 10:39:30 +0530

    card chnage commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from "react";
+import { toast } from "react-toastify";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import React, { useEffect, useState } from "react";
+import { apiUrl, filterData } from "./data";
+import Spinner from "./components/Spinner";
 
 const App = () => {
-  return <div>
-    App
-  </div>;
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+  async function fetchdata() {
+    setLoading(true);
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      setCourses(output.data);
+    } catch (error) {
+      toast.error("network is not available");
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar />
+      </div>
+      <div className="bg-bgDark2">
+        <div>
+          <Filter
+            filterData={filterData}
+            category={category}
+            setCategory={setCategory}
+          />
+        </div>
+        <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Cards courses={courses} category={category} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
